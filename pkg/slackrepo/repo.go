4f00bea741c5121220e106bcd2c58457a1007a52
Alba Rincón <alba.rincon@typeform.com>
2021-09-04 13:44:21 +0200

    Add slack repo

---
+++ pkg/slackrepo/repo.go
@@ -0,0 +1,30 @@
+package slackrepo
+
+import "database/sql"
+
+type Auth struct {
+	UserID      string `db:"user_id"`
+	TeamID      string `db:"team_id"`
+	ViewID      string `db:"view_id"`
+	AccessToken string `db:"access_token"`
+}
+
+type Repo struct {
+	db *sql.DB
+}
+
+func New(db *sql.DB) *Repo {
+	return &Repo{db: db}
+}
+
+func (r *Repo) Create(auth *Auth) error {
+	query := `
+        INSERT INTO slack_auths (user_id, team_id, view_id, access_token) 
+        VALUES ($1, $2, $3, $4)
+	`
+
+	args := []interface{}{auth.UserID, auth.TeamID, auth.ViewID, auth.AccessToken}
+	_, err := r.db.Exec(query, args...)
+
+	return err
+}
