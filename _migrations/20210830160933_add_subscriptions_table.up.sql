177f534c1bfbf6757ca7e7ca4967c3c4827b914e
Alba Rincón <alba.rincon@typeform.com>
2021-09-04 19:07:16 +0200

    Add subscriptions table

--- _migrations/20210830160933_add_subscriptions_table.up.sql
+++ _migrations/20210830160933_add_subscriptions_table.up.sql
@@ -1,7 +1,6 @@
-CREATE TABLE events
+CREATE TABLE subscriptions
 (
     subscription_id TEXT,
-    type            TEXT,
     twitch_user_id  TEXT,
     slack_user_id   TEXT,
     created_at      TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
